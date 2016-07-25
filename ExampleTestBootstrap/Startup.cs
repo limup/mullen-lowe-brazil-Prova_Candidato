eeb16770b17f5f36c73ebcf13fd0e3acb8f1198c
Limup <limup@outlook.com>
2016-07-24 22:38:09 -0300

    Add project files.

---
+++ ExampleTestBootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExampleTestBootstrap.Startup))]
+namespace ExampleTestBootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
